2f8bfe6a6a7e3cfc8068a6eb0693983f15baadb9
marahmusleh <marahaltarefe18@gmail.com>
2021-08-03 03:00:37 +0300

    weather data

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const express = require('express');
+const server = express();
+
+const weatherData = require('./data/weather.json');
+
+require('dotenv').config();
+const PORT = process.env.PORT
+
+const cors = require('cors');
+
+class Forecast {
+    constructor(value) {
+        this.valid_date = value.valid_date;
+        this.description = ` ${value.weather.description}`;
+    }
+}
+
+server.get('/', (req, res) => {
+    res.send('Hello from backend');
+});
+server.get('/weather', (req, res) => {
+
+    try {
+        let { searchQuery, lat, lon } = req.query;
+        let cityData = weatherData.find(element =>
+            element.city_name.toLowerCase() === searchQuery.toLowerCase() ||
+            (element.lat === lat && element.lon === lon) // to search by city or (lat+lon)
+        );
+        let forecastArr = cityData.data.map(items => new Forecast(items));
+        console.log(forecastArr);
+        res.send(forecastArr);
+    }
+    catch (e) {
+        res.status(404).send('No Data for this City');
+     }
+});
+
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
