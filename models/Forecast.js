4000c3db555ebe5025478f1a9c66a8b84ed6fab5
marahmusleh <marahaltarefe18@gmail.com>
2021-08-04 03:00:33 +0300

    add movie

---
+++ models/Forecast.js
@@ -0,0 +1,7 @@
+class Forecast {
+    constructor(value) {
+        this.valid_date = value.valid_date;
+        this.description = ` ${value.weather.description}`;
+    }
+}
+module.exports = Forecast;
